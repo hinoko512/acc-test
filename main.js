93dd1a2a2cda037dfb2e4093816d0a66ecb04870
奥川和希 <okugawakazuki@okugawakazukinoMacBook-Pro.local>
2022-12-06 17:24:29 +0900

    request permission

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-var count = 1;
-
-function handleMotionEvent(event) {
-  var x = event.accelerationIncludingGravity.x;
-  var y = event.accelerationIncludingGravity.y;
-  var z = event.accelerationIncludingGravity.z;
-  document.getElementById("x").innerText(x);
-  let text = document.getElementById("text");
-  if (text.textContent === "0") {
-    text.innerText = count++;
+document.getElementById("btn").addEventListener("click", () => {
+  if (window.DeviceMotionEvent && window.DeviceMotionEvent.requestPermission) {
+    DeviceMotionEvent.requestPermission()
+      .then((state) => {
+        if (state === "granted") {
+          window.addEventListener(
+            "devicemotion",
+            (event) => {
+              var x = event.accelerationIncludingGravity.x;
+              var y = event.accelerationIncludingGravity.y;
+              var z = event.accelerationIncludingGravity.z;
+              document.getElementById("x").innerText(x);
+            },
+            true
+          );
+          alert("計測を開始します。");
+        } else {
+          alert("加速度の計測を許可してください。");
+        }
+      })
+      .catch((err) => console.error(err));
+  } else {
+    alert("エラーが発生しました。");
   }
-  // Do something awesome.
-}
-
-window.addEventListener("devicemotion", handleMotionEvent, true);
+});
