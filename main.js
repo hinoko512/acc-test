7c72c6c453cc949e16b3ab8aec67fc206123776f
奥川和希 <okugawakazuki@okugawakazukinoMacBook-Pro.local>
2022-12-06 17:06:46 +0900

    add confirm text

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+var count = 1;
+
 function handleMotionEvent(event) {
   var x = event.accelerationIncludingGravity.x;
   var y = event.accelerationIncludingGravity.y;
   var z = event.accelerationIncludingGravity.z;
   document.getElementById("x").innerText(x);
+  let text = document.getElementById("text");
+  if (text.textContent === "a") {
+    text.innerText = count++;
+  }
   // Do something awesome.
 }
 
